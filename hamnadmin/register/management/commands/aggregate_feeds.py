581067ed6f5e44bd27b44e662f4bd9aa4a40a05c
Daymel Bonne <dbonne@2ndquadrant.ec>
2017-06-06 21:51:07 -0500

    Add emails entries

--- hamnadmin/register/management/commands/aggregate_feeds.py
+++ hamnadmin/register/management/commands/aggregate_feeds.py
@@ -1,15 +1,12 @@
-# from gevent import monkey
-# monkey.patch_all()
-from gevent.threadpool import ThreadPool
 import gevent
-
-from django.core.management.base import BaseCommand, CommandError
-from django.db import transaction
 from django.conf import settings
+from django.core.management.base import BaseCommand
+from django.db import transaction
+from gevent.threadpool import ThreadPool
 
+from hamnadmin.mailqueue.util import send_simple_mail
 from hamnadmin.register.models import Blog, Post, AggregatorLog
 from hamnadmin.util.aggregate import FeedFetcher
-from hamnadmin.mailqueue.util import send_simple_mail
 from hamnadmin.util.varnish import purge_root_and_feeds
 
 
@@ -86,54 +83,66 @@ class Command(BaseCommand):
                                 titles.append(entry.title)
                                 total_entries += 1
 
-                        # if entries > 0 and feed.approved:
-                        #     # Email a notification that they were picked up
-                        #     send_simple_mail(settings.EMAIL_SENDER,
-                        #                      feed.user.email,
-                        #                      "Posts found at your blog at Planet PostgreSQL",
-                        #                      u"The blog aggregator at Planet PostgreSQL has just picked up the following\nposts from your blog at {0}:\n\n{1}\n\nIf these entries are correct, you don't have to do anything.\nIf any entry should not be there, head over to\n\nhttps://planet.postgresql.org/register/edit/{2}/\n\nand click the 'Hide' button for those entries as soon\nas possible.\n\nThank you!\n\n".format(
-                        #                          feed.blogurl,
-                        #                          "\n".join(["* " + t for t in titles]),
-                        #                          feed.id),
-                        #                      sendername="Planet PostgreSQL",
-                        #                      receivername=u"{0} {1}".format(feed.user.first_name, feed.user.last_name),
-                        #                      )
-                        #
-                        # if entries > 0 and not had_entries:
-                        #     # Entries showed up on a blog that was previously empty
-                        #     send_simple_mail(settings.EMAIL_SENDER,
-                        #                      settings.NOTIFICATION_RECEIVER,
-                        #                      "A blog was added to Planet PostgreSQL",
-                        #                      u"The blog at {0} by {1}\nwas added to Planet PostgreSQL, and has now received entries.\n\nTo moderate: https://planet.postgresql.org/register/moderate/\n\n".format(
-                        #                          feed.feedurl, feed.user),
-                        #                      sendername="Planet PostgreSQL",
-                        #                      receivername="Planet PostgreSQL Moderators",
-                        #                      )
-                        #
-                        # # If the blog URL changed, update it as requested
-                        # if getattr(feed, 'new_blogurl', None):
-                        #     print("URL changed for %s to %s" % (feed.feedurl, feed.new_blogurl))
-                        #     send_simple_mail(settings.EMAIL_SENDER,
-                        #                      settings.NOTIFICATION_RECEIVER,
-                        #                      "A blog url changed on Planet PostgreSQL",
-                        #                      u"When checking the blog at {0} by {1}\nthe blog URL was updated to:\n{2}\n(from previous value {3})\n\nTo moderate: https://planet.postgresql.org/register/moderate/\n\n".format(
-                        #                          feed.feedurl, feed.user, feed.new_blogurl, feed.blogurl),
-                        #                      sendername="Planet PostgreSQL",
-                        #                      receivername="Planet PostgreSQL Moderators",
-                        #                      )
-                        #     send_simple_mail(settings.EMAIL_SENDER,
-                        #                      feed.user.email,
-                        #                      "URL of your blog at Planet PostgreSQL updated",
-                        #                      u"The blog aggregator at Planet PostgreSQL has update the URL of your blog\nwith the feed at {0} to:\n{1} (from {2})\nIf this is correct, you don't have to do anything.\nIf not, please contact planet@postgresql.org\n".format(
-                        #                          feed.feedurl,
-                        #                          feed.new_blogurl,
-                        #                          feed.blogurl,
-                        #                      ),
-                        #                      sendername="Planet PostgreSQL",
-                        #                      receivername=u"{0} {1}".format(feed.user.first_name, feed.user.last_name),
-                        #                      )
-                        #     feed.blogurl = feed.new_blogurl
-                        #     feed.save()
+                        if entries > 0 and feed.approved:
+                            # Email a notification that they were picked up
+                            send_simple_mail(settings.EMAIL_SENDER,
+                                             feed.user.email,
+                                             "Posts found at your blog at Planet PostgreSQL",
+                                             u"The blog aggregator at Planet PostgreSQL has just picked up the "
+                                             u"following\nposts from your blog at {0}:\n\n{1}\n\nIf these entries are "
+                                             u"correct, you don't have to do anything.\nIf any entry should not be "
+                                             u"there, head over to\n\nhttps://planet.postgresql.org/register/edit/"
+                                             u"{2}/\n\nand click the 'Hide' button for those entries as soon\nas "
+                                             u"possible.\n\nThank you!\n\n".format(
+                                                 feed.blogurl,
+                                                 "\n".join(["* " + t for t in titles]),
+                                                 feed.id),
+                                             sendername="Planet PostgreSQL",
+                                             receivername=u"{0} {1}".format(feed.user.first_name, feed.user.last_name),
+                                             )
+
+                        if entries > 0 and not had_entries:
+                            # Entries showed up on a blog that was previously empty
+                            send_simple_mail(settings.EMAIL_SENDER,
+                                             settings.NOTIFICATION_RECEIVER,
+                                             "A blog was added to Planet PostgreSQL",
+                                             u"The blog at {0} by {1}\nwas added to Planet PostgreSQL, and has now "
+                                             u"received entries.\n\nTo moderate: "
+                                             u"https://planet.postgresql.org/register/moderate/\n\n".format(
+                                                 feed.feedurl, feed.user),
+                                             sendername="Planet PostgreSQL",
+                                             receivername="Planet PostgreSQL Moderators",
+                                             )
+
+                        # If the blog URL changed, update it as requested
+                        if getattr(feed, 'new_blogurl', None):
+                            print("URL changed for %s to %s" % (feed.feedurl, feed.new_blogurl))
+                            send_simple_mail(settings.EMAIL_SENDER,
+                                             settings.NOTIFICATION_RECEIVER,
+                                             "A blog url changed on Planet PostgreSQL",
+                                             u"When checking the blog at {0} by {1}\nthe blog URL was updated to:\n"
+                                             u"{2}\n(from previous value {3})\n\nTo moderate: "
+                                             u"https://planet.postgresql.org/register/moderate/\n\n".format(
+                                                 feed.feedurl, feed.user, feed.new_blogurl, feed.blogurl),
+                                             sendername="Planet PostgreSQL",
+                                             receivername="Planet PostgreSQL Moderators",
+                                             )
+                            send_simple_mail(settings.EMAIL_SENDER,
+                                             feed.user.email,
+                                             "URL of your blog at Planet PostgreSQL updated",
+                                             u"The blog aggregator at Planet PostgreSQL has update the URL of your "
+                                             u"blog\nwith the feed at {0} to:\n{1} (from {2})\nIf this is correct, "
+                                             u"you don't have to do anything.\nIf not, please contact "
+                                             u"planet@postgresql.org\n".format(
+                                                 feed.feedurl,
+                                                 feed.new_blogurl,
+                                                 feed.blogurl,
+                                             ),
+                                             sendername="Planet PostgreSQL",
+                                             receivername=u"{0} {1}".format(feed.user.first_name, feed.user.last_name),
+                                             )
+                            feed.blogurl = feed.new_blogurl
+                            feed.save()
                 if self.debug:
                     # Roll back transaction without error
                     raise BreakoutException()
@@ -155,5 +164,5 @@ class Command(BaseCommand):
             entries = list(fetcher.parse(since))
         except Exception as e:
             self.stderr.write("Failed to fetch '%s': %s" % (fetcher.feed.feedurl, e))
-            return (fetcher.feed, e)
-        return (fetcher.feed, entries)
+            return fetcher.feed, e
+        return fetcher.feed, entries
